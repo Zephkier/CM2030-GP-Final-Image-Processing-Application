0b70fc9cd8c290b359f4d4340cc119cbb9d641de
Benjamin <benjamin_kok@hotmail.com>
2024-02-18 13:43:17 +0800

    First commit: Create basic capture grids

---
+++ sketch.js
@@ -0,0 +1,308 @@
+let capture;
+let setWidth = 640 / 4; // minimum = 160
+let setHeight = 480 / 4; // minimum = 120
+let margin = 20;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  angleMode(DEGREES);
+
+  pixelDensity(1);
+  capture = createCapture(VIDEO);
+  capture.size(setWidth, setHeight);
+  capture.hide();
+}
+
+function draw() {
+  background(20);
+
+  // reference: positions = [ [{x,y}, {x,y}, {x,y}], repeat 4 more times ]
+  let positions = [];
+  for (let i = 0; i < 5; i++) {
+    positions[i] = [];
+    for (let j = 0; j < 3; j++) {
+      positions[i][j] = {
+        x: margin + j * (setWidth + margin),
+        y: margin + i * (setHeight + margin),
+      };
+    }
+  }
+
+  // Row 1 DONE
+  image(capture, positions[0][0].x, positions[0][0].y, setWidth, setHeight);
+  captureEditGrey(capture, positions[0][1].x, positions[0][1].y, setWidth, setHeight);
+
+  // Row 2 DONE
+  captureEditR(capture, positions[1][0].x, positions[1][0].y, setWidth, setHeight);
+  captureEditG(capture, positions[1][1].x, positions[1][1].y, setWidth, setHeight);
+  captureEditB(capture, positions[1][2].x, positions[1][2].y, setWidth, setHeight);
+
+  // Row 3 TODO
+  captureEditSegment1(capture, positions[2][0].x, positions[2][0].y, setWidth, setHeight);
+  captureEditSegment2(capture, positions[2][1].x, positions[2][1].y, setWidth, setHeight);
+  captureEditSegment3(capture, positions[2][2].x, positions[2][2].y, setWidth, setHeight);
+
+  // Row 4 TODO
+  captureEditRepeat(capture, positions[3][0].x, positions[3][0].y, setWidth, setHeight); // DONE ?
+  captureEditColourSpace1(capture, positions[3][1].x, positions[3][1].y, setWidth, setHeight);
+  captureEditColourSpace2(capture, positions[3][2].x, positions[3][2].y, setWidth, setHeight);
+
+  // Row 5 TODO
+  captureEditFaceDetect(capture, positions[4][0].x, positions[4][0].y, setWidth, setHeight);
+  captureEditColourSpace1Segment(capture, positions[4][1].x, positions[4][1].y, setWidth, setHeight);
+  captureEditColourSpace2Segment(capture, positions[4][2].x, positions[4][2].y, setWidth, setHeight);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+/*
+NOTE
+actual 'capture' is at position (0, 0)
+image() simply draws it at position (margin, margin)
+thus, have to copy from (0, 0) to (0, 0), then simply move the copy using image()
+*/
+
+// Row 1
+function captureEditGrey(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      let grey = (chanR + chanG + chanB) / 3;
+      // Value cannot go over 255
+      let bright = min(255, grey * 1.2);
+      captureCopy.pixels[index + 0] = bright;
+      captureCopy.pixels[index + 1] = bright;
+      captureCopy.pixels[index + 2] = bright;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+// Row 2
+function captureEditR(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = 0;
+      let chanB = 0;
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditG(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = 0;
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = 0;
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditB(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = 0;
+      let chanG = 0;
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+// Row 3
+function captureEditSegment1(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditSegment2(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditSegment3(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+// Row 4
+function captureEditRepeat(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditColourSpace1(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditColourSpace2(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+// Row 5
+function captureEditFaceDetect(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditColourSpace1Segment(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
+
+function captureEditColourSpace2Segment(src, x, y, w, h) {
+  let captureCopy = createImage(setWidth, setHeight);
+  captureCopy.copy(src, 0, 0, setWidth, setHeight, 0, 0, setWidth, setHeight);
+  captureCopy.loadPixels();
+  for (let x = 0; x < captureCopy.width; x++) {
+    for (let y = 0; y < captureCopy.height; y++) {
+      let index = (captureCopy.width * y + x) * 4;
+      let chanR = captureCopy.pixels[index + 0];
+      let chanG = captureCopy.pixels[index + 1];
+      let chanB = captureCopy.pixels[index + 2];
+      captureCopy.pixels[index + 0] = chanR;
+      captureCopy.pixels[index + 1] = chanG;
+      captureCopy.pixels[index + 2] = chanB;
+    }
+  }
+  captureCopy.updatePixels();
+  image(captureCopy, x, y, w, h);
+}
